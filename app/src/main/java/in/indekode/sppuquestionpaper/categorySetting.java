564c668edb97235d36f082a5ce7b0f15d470a7a8
arvindbadwar19 <arvind.badwar19@gmail.com>
2018-06-04 09:39:37 +0530

    Changes in CategoryActivity Class.

--- app/src/main/java/in/indekode/sppuquestionpaper/categorySetting.java
+++ app/src/main/java/in/indekode/sppuquestionpaper/categorySetting.java
@@ -2,12 +2,35 @@ package in.indekode.sppuquestionpaper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class categorySetting extends AppCompatActivity {
+
+    Spinner spinnerYear = (Spinner) findViewById(R.id.spinner_year);
+    Spinner spinnerBranch = (Spinner) findViewById(R.id.spinner_branch);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_setting);
+
+        ArrayAdapter<CharSequence> staticAdapterYear = ArrayAdapter
+                .createFromResource(this, R.array.choiceYear,
+                        android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> staticAdapterBranch = ArrayAdapter
+                .createFromResource(this, R.array.choiceBranch,
+                        android.R.layout.simple_spinner_item);
+
+        staticAdapterYear
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        staticAdapterBranch
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinnerYear.setAdapter(staticAdapterYear);
+        spinnerBranch.setAdapter(staticAdapterBranch);
+
     }
 }
