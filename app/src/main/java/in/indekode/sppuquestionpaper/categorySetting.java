a08f159f8126c0484b161521e00d70a9c43e82dc
arvindbadwar19 <arvind.badwar19@gmail.com>
2018-06-04 09:22:18 +0530

    new activity added : "categorySetting"

---
+++ app/src/main/java/in/indekode/sppuquestionpaper/categorySetting.java
@@ -0,0 +1,13 @@
+package in.indekode.sppuquestionpaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class categorySetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_setting);
+    }
+}
